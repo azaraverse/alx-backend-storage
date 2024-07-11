f2319d6ddcae5cdd992bb8dcac07336c4e92ae1b
Azara <yyushahu02@gmail.com>
2024-07-11 17:04:28 +0000

    push div function to container for testing

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,26 @@
+-- A SQL script that creates a function SafeDiv that divides the first by the second number
+
+-- Create function
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS INT
+BEGIN
+    -- Declare variables
+    DECLARE numerator INT;
+    DECLARE denominator INT;
+    DECLARE result INT;
+
+    -- Set arguments to declared variables
+    SET numerator = a;
+    SET denominator = b;
+
+    -- check if the denominator equals 0
+    IF denominator = 0 THEN
+        RETURN 0;
+    -- Perform division if denominator passes
+    ELSE SET result = numerator / denominator;
+    END IF;
+
+    RETURN result;
+
+END $$
+DELIMITER ;
