10fc14a1965aa91839fffee6fb7a78ac106885fd
Yushahu <yyushahu02@gmail.com>
2024-07-11 18:13:54 +0000

    fix average weight sum and division, procedure name and procedure drops for existing procedures

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,13 +1,15 @@
 -- A SQL script that creates a stored procedure ComputeAverageScoreForUser() that computes and stores the average score for a student.
 
 DELIMITER $$
+-- Drop stored procedure if exists
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 -- Create stored procedure
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     -- Update the users table with the computed weighted average score
     UPDATE users
     SET average_score = (
-        SELECT SUM(corrections.score * projects.weight)
+        SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
         FROM corrections, projects
         WHERE corrections.user_id = user_id
         AND corrections.project_id = projects.id
