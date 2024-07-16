8d531ae781c8a0f891bf0028fcf5a6d6ed477a7b
Azara <yyushahu02@gmail.com>
2024-07-16 22:08:29 +0000

    document, refactor and type-annotate the script

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
-""""""
+"""
+A Python script that provides some stats about Nginx logs stored in
+MongoDB
+"""
 from pymongo import MongoClient
 
 
 def log_stats(collection):
-    """"""
-    logs_count = collection.count_documents({})
+    """Displays statistics about Nginx logs stored in MongoDB"""
+    logs_count: int = collection.count_documents({})
     print(f'{logs_count} logs')
 
     # method document count
-    get_count = collection.count_documents({'method': {'$eq': 'GET'}})
-    post_count = collection.count_documents({'method': {'$eq': 'POST'}})
-    put_count = collection.count_documents({'method': {'$eq': 'PUT'}})
-    patch_count = collection.count_documents({'method': {'$eq': 'PATCH'}})
-    delete_count = collection.count_documents(
+    get_count: int = collection.count_documents(
+        {'method': {'$eq': 'GET'}}
+    )
+    post_count: int = collection.count_documents(
+        {'method': {'$eq': 'POST'}}
+    )
+    put_count: int = collection.count_documents(
+        {'method': {'$eq': 'PUT'}}
+    )
+    patch_count: int = collection.count_documents(
+        {'method': {'$eq': 'PATCH'}}
+    )
+    delete_count: int = collection.count_documents(
         {'method': {'$eq': 'DELETE'}}
     )
     print('Methods:')
@@ -23,14 +34,14 @@ def log_stats(collection):
     print(f'\tmethod PATCH: {patch_count}')
     print(f'\tmethod DELETE: {delete_count}')
 
-    method_path = collection.count_documents(
+    method_path: int = collection.count_documents(
         {'path': {'$eq': '/status'}}
     )
     print(f'{method_path} status check')
 
 
 if __name__ == '__main__':
-    """"""
+    """Run client"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
