33a09b34fe2bc356374139b9af4aa7f575916e3c
Azara <yyushahu02@gmail.com>
2024-07-16 20:43:20 +0000

    store id in a variable

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -12,5 +12,5 @@ def insert_school(mongo_collection, **kwargs):
     Returns:
         Object: The new _id object
     """
-    mongo_collection.insert_one(kwargs)
-    return mongo_collection.inserted_id
+    id = mongo_collection.insert_one(kwargs)
+    return id.inserted_id
