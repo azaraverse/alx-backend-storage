2f9c85c4ddfc518402e5475f18890b70d50c6080
Azara <yyushahu02@gmail.com>
2024-07-16 21:06:22 +0000

    add python script for finding a match to container for testing

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Find a match"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    A function that returns a list of schools having a specific topic
+
+    Args:
+        mongo_collection: pymongo collection object
+        topic (str): topic to search
+    """
+    return mongo_collection.find({'topic': {'$eq': topic}})
