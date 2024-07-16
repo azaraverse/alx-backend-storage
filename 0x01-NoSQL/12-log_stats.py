850225d696fc352fe6294911f5c69660505494da
Azara <yyushahu02@gmail.com>
2024-07-16 21:27:40 +0000

    push log stats to test the log count feature on the remote container

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""""""
+from pymongo import MongoClient
+
+
+def log_stats(collection):
+    """"""
+    logs_count = collection.count()
+    print(f'{logs_count} logs')
+
+
+if __name__ == '__main__':
+    """"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    log_stats(collection=collection)
