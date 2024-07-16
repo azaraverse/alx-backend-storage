056433e603b16e604adc187d82a112aff4a201a6
Azara <yyushahu02@gmail.com>
2024-07-16 23:15:51 +0000

    push top_students aggregation pipeline for testing on remote container

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""""""
+
+
+def top_students(mongo_collection):
+    """"""
+    top_students = mongo_collection.aggregate([
+        {
+            '$group': {'_id': '$_id', 'name': '$name', 'averageScore': {'$avg': '$score'}}
+        },
+        {
+            '$sort': {'averageScore': -1}
+        }
+    ])
+
+    return top_students
