d13cc1f6cd359356741edda5a70cd2baa7d17ffb
Azara <yyushahu02@gmail.com>
2024-07-16 23:29:10 +0000

    re-structure script to abide by pycodestyle recommendations

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-""""""
+"""Aggregation pipelines"""
 
 
 def top_students(mongo_collection):
-    """"""
+    """
+    A function that uses aggregation pipeline to return sorted
+    average scores of students.
+    """
     top_students = mongo_collection.aggregate([
         {
             '$unwind': '$topics'
         },
         {
-            '$group': {'_id': '$_id', 'name': {'$first':'$name'}, 'averageScore': {'$avg': '$topics.score'}}
+            '$group': {
+                '_id': '$_id',
+                'name': {'$first': '$name'},
+                'averageScore': {'$avg': '$topics.score'}
+            }
         },
         {
             '$sort': {'averageScore': -1}
