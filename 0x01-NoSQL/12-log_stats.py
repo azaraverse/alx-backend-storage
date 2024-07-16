3ced22e33005228336afa08be4304f95944f54c5
Azara <yyushahu02@gmail.com>
2024-07-16 21:53:57 +0000

    implement the get method document count for testing

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,6 +8,10 @@ def log_stats(collection):
     logs_count = collection.count_documents({})
     print(f'{logs_count} logs')
 
+    # get method document count
+    get_count = collection.count_documents({'method': {'$eq': 'GET'}})
+    print(f'\tmethod GET: {get_count}')
+
 
 if __name__ == '__main__':
     """"""
